1f6b76dbe0b2c95e12a66f4b5bad9bb76d712d4b
Lisa Trevis <l_trevis@hotmail.com>
2017-06-08 21:18:45 -0700

    Initial commit

---
+++ includes/arrays.php
@@ -0,0 +1,89 @@
+<?php
+	
+	// Nav menu items
+	$navItems = array(
+
+					array(
+						slug => "index.php",
+						title=> "Home"
+					),
+					array(
+						slug => "team.php",
+						title=> "Team"
+					),
+					array(
+						slug => "menu.php",
+						title=> "Menu"
+					),
+					array(
+						slug => "contact.php",
+						title=> "Contact"
+					),
+		);
+
+	// Team Members
+	$teamMembers = array(
+
+						array(
+
+							name 		=> "Frankie III",
+							position 	=> "Owner",
+							bio 		=> "Frankie is the great-grandson of the original Franklin. He is the owner of Franklin's Fine Dining. He cooks a mean fritata!",
+							img 		=> "frankie"
+
+						),
+
+						array(
+
+							name 		=> "Francis",
+							position 	=> "General Manager",
+							bio 		=> "Francis knows her stuff. The big sister of Frankie himself, she runs the show. Don't miss her Margarita Mondays!",
+							img 		=> "francis"
+
+						),
+
+						array(
+
+							name 		=> "Carlos",
+							position 	=> "Head Chef",
+							bio 		=> "Carlos is the epitome of the phrase &ldquo;Don't judge a book by its cover&rdquo; &mdash; You simply cannot find a better chef.",
+							img 		=> "carlos"
+
+						),
+
+		);
+
+	// Menu items
+	$menuItems = array(
+
+					"club-sandwich" => array(
+						title => "Club Sandwich",
+						price => 11,
+						blurb => "A club sandwich, also called a clubhouse sandwich, is a sandwich of occasionally toasted bread, sliced cooked poultry, fried bacon, lettuce, tomato, and mayonnaise. It is often cut into quarters or halves and held together by hors d'œuvre sticks.",
+						drink => "Club Soda"
+						),
+
+					"dill-salmon" => array(
+						title => "Lemon &amp; Dill Salmon",
+						price => 18,
+						blurb => "This simple fish dish is best made with wild salmon, but it works equally well with the farmed sort. Don't be afraid to play with herb and fat combinations: parsley, chervil or dill work well with salmon.",
+						drink => "Fancy Wine"
+						),
+
+					"super-salad" => array(
+						title => "The Super Salad<sup>&reg;</sup>",
+						price => 34,
+						blurb => "Want to discover a simple, delicious way to get your four servings of vegetables per day? Fix a salad. Start with the right kind of greens. Choose from a broad range of veggies – or even fruit – to add color, texture and flavor. Include a healthy source of protein to satisfy your appetite. Then top it off with heart-healthy dressing. Below are tips for creating healthy, beautiful salads you can celebrate.",
+						drink => "Jug o' Water"
+						),
+
+					"mexican-barbacoa" => array(
+						title => "Mexican Barbacoa",
+						price => 23,
+						blurb => "Mexican barbacoa is meat prepared in a way that allows it to be very tender when it's cooked, and shred very easily. The traditional way to make it is with lamb or goat meat, but you will also find it with beef.",
+						drink => "Beer with a Lime"
+						),
+					
+		);
+
+?>
---
+++ menu.php
@@ -0,0 +1,32 @@
+<?php
+
+	define("TITLE", "Page Menu | Franklin's Fine Dining");
+
+	include("includes/header.php");
+
+?>
+
+	<div id="menu-items">
+		
+		<h1>Our Delicious Menu</h1>
+		<p>Like our Team, our menu is very small &mdash; but dang, does it ever pack a punch!</p>
+		<p><em>Click any menu item to learn more about it.</em></p>
+
+		<hr>
+
+		<ul>
+			<?php foreach($menuItems as $dish => $item) { ?>
+
+				<li><a href="dish.php?item=<?php echo $dish; ?>"><?php echo $item[title]; ?></a> <sup>$</sup><?php echo $item[price]; ?></li>
+
+				<?php } ?>
+
+		</ul>
+
+	</div><!-- menu items -->
+
+<?php
+
+	include("includes/footer.php");
+
+?>
